60cdb7b68b60150fa425679b1f66fa2ee829211c
Trazius Biz <traziusbiz@gmail.com>
2015-01-19 03:53:29 +0200

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$('a[href^="#"]').click(function(){
+//Сохраняем значение атрибута href в переменной:
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 400);
+return false;
+});
