95036474c93594b2a56a82b582c33cb62463eda3
Trazius Biz <traziusbiz@gmail.com>
2015-01-26 03:05:09 +0200

    add active menu script

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,63 @@ $(window).load(function(){
          return false;
     });
 });
+
+
+//dropdown menu
+$(function() {
+  $('.icon').click(function() {
+    if($('.items').is(':visible')) {
+      $('.items').removeClass('showitems');
+    }
+    else {
+      $('.items').addClass('showitems');
+    }  
+ });
+});
+
+
+// Click event
+offset = 0;
+    $('#menu li a[href^="#"]').click(function(event) {
+
+        // Prevent from default action to intitiate
+        event.preventDefault();
+         
+        //remove active from all anchor and add it to the clicked anchor
+        $('#menu li a[href^="#"]').removeClass("active")
+        $(this).addClass('active');
+        
+        // The id of the section we want to go to
+        var anchorId = $(this).attr('href');
+
+        // Our scroll target : the top position of the section that has the id referenced by our href
+        var target = $(anchorId).offset().top - offset;
+        console.log(target);
+
+        $('html, body').stop().animate({ scrollTop: target }, 500, function () {
+            window.location.hash = anchorId;
+        });
+
+        return false;
+    });
+
+//check the pages when scroll event occurs
+$(window).scroll(function(){
+    // Get the current vertical position of the scroll bar
+    position = $(this).scrollTop();
+    $('#menu li a[href^="#"]').each(function(){
+          var anchorId = $(this).attr('href'); 
+           var target = $(anchorId).offset().top - offset;
+           // check if the document has crossed the page
+        console.log(position,target);
+        if(position>=target){
+             //remove active from all anchor and add it to the clicked anchor
+            $('#menu li a[href^="#"]').removeClass("active")
+            $(this).addClass('active'); 
+        }
+    })
+})
+$(function(){
+    //set our scroll state after dom ready
+    $(window).scroll();
+})
